7e93fc6851c67090129773891a7f59bb2568e485
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-07-13 15:39:46 +0200

    Add mvvm base pattern

---
+++ Untis Desktop/ViewModels/DelegateCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace UntisDesktop.ViewModels;
+
+/// <summary>
+/// A command to run in MVVM
+/// </summary>
+internal class DelegateCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    /// <summary>
+    /// Command to execute
+    /// </summary><
+    private readonly Action<object> _execute;
+
+    /// <summary>
+    /// Function to check if command is executable
+    /// </summary>
+    private readonly Predicate<object> _canExecute;
+
+    /// <summary>
+    /// Define a command that run without any condition
+    /// </summary>
+    /// <param name="execute">Action to run</param>
+    public DelegateCommand(Action<object> execute) : this(new Predicate<object>(_ => true), execute)
+    {
+    }
+
+    /// <summary>
+    /// Define a command that run with a condition
+    /// </summary>
+    /// <param name="canExecute">Condition</param>
+    /// <param name="execute">Action to run when condition is <see langword="true"/></param>
+    public DelegateCommand(Predicate<object> canExecute, Action<object> execute)
+    {
+        _canExecute = canExecute;
+        _execute = execute;
+    }
+
+    /// <summary>
+    /// Update source button
+    /// </summary>
+    public void RaiseCanExecuteChanged() =>
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    bool ICommand.CanExecute(object? parameter) =>
+        _canExecute.Invoke(parameter ?? new object());
+
+    void ICommand.Execute(object? parameter) =>
+        _execute.Invoke(parameter ?? new object());
+}
---
+++ Untis Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace UntisDesktop.ViewModels;
+
+/// <summary>
+/// The base of every view model
+/// </summary>
+internal abstract class ViewModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
+{
+    public bool HasErrors =>
+        Errors.Count > 0;
+
+    /// <summary>
+    /// Get current View model base
+    /// </summary>
+    public ViewModelBase Error =>
+        this;
+
+    private IDictionary<string, List<string>> Errors { get; } = new Dictionary<string, List<string>>();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    /// <summary>
+    /// Update a property 
+    /// </summary>
+    /// <param name="propertyName">Property to update</param>
+    protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        if (!string.IsNullOrEmpty(propertyName))
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public bool HasError([CallerMemberName] string propertyName = "") =>
+        Errors.ContainsKey(propertyName);
+
+    public IEnumerable GetErrors([CallerMemberName] string? propertyName = "")
+    {
+        if (Errors.TryGetValue(propertyName ?? string.Empty, out _))
+            return Errors[propertyName ?? string.Empty];
+        else
+            return Array.Empty<string>();
+    }
+
+    /// <summary>
+    /// Get the next error for the property
+    /// </summary>
+    /// <param name="propertyName">Property</param>
+    /// <returns>Error message</returns>
+    public string this[string propertyName]
+    {
+        get => GetErrors(propertyName).Cast<string>().First();
+    }
+
+    /// <summary>
+    /// Add an error to the error list for the property
+    /// </summary>
+    /// <param name="message">Error message</param>
+    /// <param name="propertyName">Property</param>
+    protected virtual void AddError(string message, [CallerMemberName] string propertyName = "")
+    {
+        if (!Errors.ContainsKey(propertyName))
+            Errors.Add(propertyName, new List<string>());
+
+        Errors[propertyName].Add(message);
+    }
+
+    /// <summary>
+    /// Remove all errors for the property
+    /// </summary>
+    /// <param name="propertyName">Property</param>
+    protected virtual void ClearErrors([CallerMemberName] string propertyName = "")
+    {
+        Errors.Remove(propertyName);
+        RaiseErrorsChanged(propertyName);
+    }
+
+    /// <summary>
+    /// Notify that the errors has been changed
+    /// </summary>
+    /// <param name="propertyName">Property</param>
+    protected virtual void RaiseErrorsChanged([CallerMemberName] string propertyName = "")
+    {
+        if (!string.IsNullOrEmpty(propertyName))
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+
+        RaisePropertyChanged(nameof(Error));
+    }
+}
